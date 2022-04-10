10b85daf9933b09a195622cacded650ef9c55ba7
Anthony G <agithaka@live.com>
2022-04-10 13:51:40 +0000

    config visible

---
+++ src/config/navigation.js
@@ -0,0 +1,8 @@
+const main = [
+    { name: "🏠", href: "/" },
+    { name: "Movies", href: "movies" },
+    { name: "Dad Jokes", href: "dad-jokes" },
+    { name: "Github Users", href: "github-users" },
+  ];
+
+export { main };
