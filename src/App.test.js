b4becd3e4433b4053b3bc9602e92ad4d250ff0aa
Anthony G <agithaka@live.com>
2022-06-30 22:15:18 +0300

    App tets

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders home link', () => {
+//   render(<App />);
+// });
+
+// sample
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
 });
