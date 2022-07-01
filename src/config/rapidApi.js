2d454dcaeb2c4820b9528ad80cbe61437a8a5e8b
Anthony G <agithaka@live.com>
2022-07-01 13:05:54 +0300

    React App Env Var issue

--- src/config/rapidApi.js
+++ src/config/rapidApi.js
@@ -1,11 +1,8 @@
-module.exports =
-{
-  adMovies: {
-    "X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
-    "X-RapidAPI-Key": `${process.env.RAPIDAPI_APP_DEFAULT_KEY}`
-  },
-  dadJokes: {
-    "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
-    "X-RapidAPI-Key": `${process.env.RAPIDAPI_APP_DEFAULT_KEY}`
-  }
-}
+export const adMovies = {
+  'X-RapidAPI-Host': "advanced-movie-search.p.rapidapi.com",
+  'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_APP_DEFAULT_KEY
+};
+export const dadJokes = {
+  'X-RapidAPI-Host': "dad-jokes.p.rapidapi.com",
+  'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_APP_DEFAULT_KEY
+};
