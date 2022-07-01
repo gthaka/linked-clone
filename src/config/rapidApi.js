7a53d271491ed269cada6791424cf9632f1045d7
Anthony G <agithaka@live.com>
2022-07-01 12:23:33 +0300

    Rapid - Config

--- src/config/rapidApi.js
+++ src/config/rapidApi.js
@@ -2,10 +2,10 @@ module.exports =
 {
   adMovies: {
     "X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
-    "X-RapidAPI-Key": process.env.RAPIDAPI_APP_DEFAULT_KEY
+    "X-RapidAPI-Key": `${process.env.RAPIDAPI_APP_DEFAULT_KEY}`
   },
   dadJokes: {
     "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
-    "X-RapidAPI-Key": process.env.RAPIDAPI_APP_DEFAULT_KEY
+    "X-RapidAPI-Key": `${process.env.RAPIDAPI_APP_DEFAULT_KEY}`
   }
 }
