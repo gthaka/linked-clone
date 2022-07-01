9a89db28a421fd1a352de86ed69c08006e8d92d0
Anthony G <agithaka@live.com>
2022-07-01 12:03:25 +0300

    config for api header json

---
+++ src/config/rapidApi.js
@@ -0,0 +1,11 @@
+module.exports =
+{
+  adMovies: {
+    "X-RapidAPI-Host": "advanced-movie-search.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.RAPIDAPI_APP_DEFAULT_KEY
+  },
+  dadJokes: {
+    "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.RAPIDAPI_APP_DEFAULT_KEY
+  }
+}
