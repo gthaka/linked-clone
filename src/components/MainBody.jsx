3b2d490f082334f94fb9e304310eb2cb7739259f
Anthony G <agithaka@live.com>
2022-07-01 15:07:09 +0300

    jsx notation

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -5,9 +5,7 @@ export default function MainBody() {
     <main>
       <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <div className="px-4 py-6 sm:px-0">
-          <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
-            
-          </div>
+          <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
         </div>
       </div>
     </main>
