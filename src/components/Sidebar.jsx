19ad2854ef29fb0bb470b25fe834bcfbe25a3e23
Anthony G <agithaka@live.com>
2022-07-03 23:57:07 +0300

    Format

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,8 +6,8 @@ export default function Sidebar() {
   return (
     <>
       {!isOpen ? (
-        <button className="top-0 right-0 fixed text-blue" onClick={()=>setIsOpen(!isOpen)}>
-          <svg 
+        <button className="top-0 right-0 fixed text-blue" onClick={() => setIsOpen(!isOpen)}>
+          <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
             fill="none"
@@ -24,12 +24,12 @@ export default function Sidebar() {
         </button>
       ) : (
         <div className="top-0 right-0 fixed bg-blue-500 w-[35vw]">
-            <button  onClick={()=>setIsOpen(!isOpen)} className="text-xl text-white fixed top-4 right-4">
+          <button onClick={() => setIsOpen(!isOpen)} className="text-xl text-white fixed top-4 right-4">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-  <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-</svg>
-            </button>
-            &nbsp;
+              <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+            </svg>
+          </button>
+          &nbsp;
           <h2 className="text-2xl text-white">Sidebbar Components</h2>
         </div>
       )}
