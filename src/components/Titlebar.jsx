e8b712aa35497dbe3e6c3a7c96673eb60821b4eb
Anthony G <agithaka@live.com>
2022-07-03 23:57:30 +0300

    Cleanup

--- src/components/Titlebar.jsx
+++ src/components/Titlebar.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-export default function Titlebar({title,others}) {
-  // console.log(others);
+export default function Titlebar({ title, others }) {
   let button = '';
-  
-  if(others && others.button){
+
+  if (others && others.button) {
     button = others.button;
   }
   return (
     <header className="bg-white shadow">
       <div className="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold text-gray-900">{title}{button}</h1>
-        
       </div>
     </header>
   );
