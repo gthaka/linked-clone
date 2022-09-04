b8d601552498689bf33eb17bf77717a55245ea01
Anthony G <agithaka@live.com>
2022-09-04 18:27:30 +0300

    fix: heroicon version change

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
-import { CodeIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import { CodeIcon, MenuIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import { main } from "../../config/navigation";
 import { NavLink } from "react-router-dom";
 
@@ -88,7 +88,7 @@ export default function Home() {
                     <div className="-mr-2">
                       <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                         <span className="sr-only">Close main menu</span>
-                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                       </Popover.Button>
                     </div>
                   </div>
