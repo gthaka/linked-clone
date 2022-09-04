45593b1c366b919ea6bfbdeef77bf7f015309d8e
Anthony G <agithaka@live.com>
2022-09-04 18:32:12 +0300

    fix: Circle CI failing @ build

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,7 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
-import { ChevronUpDownIcon, MenuIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { ChevronUpDownIcon, Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { main } from "../../config/navigation";
 import { NavLink } from "react-router-dom";
 
@@ -44,7 +44,7 @@ export default function Home() {
                     <div className="-mr-2 flex items-center md:hidden">
                       <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                         <span className="sr-only">Open main menu</span>
-                        <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                       </Popover.Button>
                     </div>
                   </div>
