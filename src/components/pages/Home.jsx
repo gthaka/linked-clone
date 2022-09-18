2c1f9bc95f27e673db25ef6bfa61e75407ca2b50
Anthony G <agithaka@live.com>
2022-09-18 12:17:35 -0700

    Update Home.jsx
    HeroIcon
    NavLink change

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,3 @@
-/* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { ChevronUpDownIcon, Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
@@ -28,18 +27,13 @@ export default function Home() {
               >
                 <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                   <div className="flex items-center justify-between w-full md:w-auto">
-                    <NavLink to="\">
+                    <NavLink to="/">
                       <span className="sr-only">Workflow</span>
-                      <ChevronUpDownIcon
+                      <CodeBracketIcon
                         as="img"
                         className="h-8 w-auto sm:h-10 text-indigo-500"
                         fill="currentColor"
                       />
-                      {/* <img
-                        className="h-8 w-auto sm:h-10"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt="🏘️"
-                      /> */}
                     </NavLink>
                     <div className="-mr-2 flex items-center md:hidden">
                       <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
